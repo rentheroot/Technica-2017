f106fde0605cb344e3638f377d95dcb54ce9f9fb
xXReneeXx <rjs2018@gmail.com>
2017-11-04 21:47:25 -0400

    Added While loops

--- InterviewApp.py
+++ InterviewApp.py
@@ -1,4 +1,17 @@
-#---which mode--#
+#--------------------Demographics--------------#
+#---Gender---#
+def genderSelect ():
+
+    while True:
+        gender = input("What gender are you? (female, male, or other")
+        gender2 = gender.lower()
+        if gender2 == "female" or "male" or "other":
+            break
+        else:
+            print("sorry, I didn't understand that. Please type either 'female', 'male', or 'other'
+
+    
+#---------------------Which Mode---------------#
 def modeSelect ():
     mode = input("Which mode would you like to use? (easy or hard)")
     mode2 = mode.lower()
@@ -8,5 +21,9 @@ def modeSelect ():
     #hard-mode
     elif mode2 == "hard":
         print("daayum hard")
+
+    #bad-choice
+    else:
+        print("Sorry,I didn't understand that. Please type either 'easy' or 'hard'") 
 
 modeSelect()
