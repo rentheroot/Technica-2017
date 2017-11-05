dfa26ef028236a41418016e8d91d98342d5485aa
xXReneeXx <rjs2018@gmail.com>
2017-11-04 22:21:22 -0400

    added a print result thing

--- InterviewApp.py
+++ InterviewApp.py
@@ -1,16 +1,20 @@
+global gender2
+
 #--------------------Demographics--------------#
 #---Gender---#
-def genderSelect ():
-
+def genderSelect (gender2):
     while True:
-        gender = input("What gender are you? (female, male, or other")
+        gender = input("What gender are you? (female, male, or other)")
+        
         gender2 = gender.lower()
         if gender2 == "female" or "male" or "other":
+            return gender2
             break
         else:
-            print("sorry, I didn't understand that. Please type either 'female', 'male', or 'other'
+            print("sorry, I didn't understand that. Please type either 'female', 'male', or 'other'")
 
     
+                  
 #---------------------Which Mode---------------#
 def modeSelect ():
     mode = input("Which mode would you like to use? (easy or hard)")
@@ -25,5 +29,12 @@ def modeSelect ():
     #bad-choice
     else:
         print("Sorry,I didn't understand that. Please type either 'easy' or 'hard'") 
+#--declare gender variable--#
+gender2 = 0
 
+#--print gender variable--#
+gender2 = genderSelect(gender2)
+print(gender2)
+
+#--run mode select--#
 modeSelect()
