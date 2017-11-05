ed95e76f635d290bcc17ea2fad5cfb7c77c25b3e
xXReneeXx <rjs2018@gmail.com>
2017-11-04 23:45:33 -0400

    Fixed some issues and added some stuff

--- InterviewApp.py
+++ InterviewApp.py
@@ -5,17 +5,17 @@
 def genderSelect (gender2):
     #while good input isn't given
     while True:
-        gender = input("What gender are you? (female, male, or other)")
+        gender = input("What gender are you? (female, male, or other)" + '\n')
 
         gender2 = gender.lower()
 
         #if good input
-        if gender2 == "female" or "male" or "other":
+        if gender2 == "female" or gender2 == "male" or gender2 == "other":
             return gender2
             break
         #if bad input
         else:
-            print("sorry, I didn't understand that. Please type either 'female', 'male', or 'other'")
+            print("sorry, I didn't understand that. Please type either 'female', 'male', or 'other'" + '\n')
 
 #---Race--#
 def raceSelect (race2):
@@ -26,17 +26,38 @@ def raceSelect (race2):
         race2 = race.lower()
 
         #if good input
-        if race2 == "1" or "2" or "3" or "4" or "5" or "6":
+        if race2 == "1" or race2 == "2" or race2 == "3" or race2 == "4" or race2 == "5" or race2 == "6":
             return race2
             break
         #if bad input
         else:
-            print("sorry, I didn't understand that. Please type a number 1-6")
-            
+            print("sorry, I didn't understand that. Please type a number 1-6" + '\n')
+
+#---State Select--#
+def stateSelect (state2):
+
+    #list of U.S. state abbreviations
+    states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    while True:
+        state = input("Please type in the 2 letter abbreviation for the U.S. state you live in" + '\n')
+        state2 = state.upper()
+
+        #if good input
+        if state2 in states:
+            return state2
+            break
+        #if bad input
+        else:
+            print("sorry, I don't understand what you are saying, Please type in the 2 letter abbreviation for the U.S. state you live in" + '\n')
+               
 #---------------------Which Mode---------------#
 def modeSelect (mode2):
     while True:
-        mode = input("Which mode would you like to use? (easy or hard)")
+        mode = input("Which mode would you like to use? (easy or hard)" + '\n')
         mode2 = mode.lower()
 
         #if easy-mode
@@ -52,16 +73,20 @@ def modeSelect (mode2):
 
         #if bad-choice
         else:
-            print("Sorry,I didn't understand that. Please type either 'easy' or 'hard'") 
+            print("Sorry,I didn't understand that. Please type either 'easy' or 'hard'" + '\n') 
 #--declare variables--#
 gender2 = 0
 mode2 = 0
 race2 = 0
+state2 = 0
 
 #--print gender variable--#
 gender2 = genderSelect(gender2)
 print(gender2)
 
+#--print state variable--#
+state2 = stateSelect(state2)
+print(state2)
 #--run race select--#
 race2 = raceSelect(race2)
 print(race2)
